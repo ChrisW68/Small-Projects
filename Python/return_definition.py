559e577f14481599c7b3d27c1b1480165b52a74f
Chris W <chris.wiseman74@gmail.com>
2017-08-09 20:11:47 -0700

    Added new program
    New program (return_definition) will allow user to input a word and the
    program will verify input is correct and give a definition of the word
    from a list

---
+++ Python/return_definition.py
@@ -0,0 +1,42 @@
+import json
+from difflib import get_close_matches
+
+def main():
+    data = json.load(open("data.json"))
+    word = input("Enter a word you want definition on: ").lower()
+    from difflib import SequenceMatcher
+
+    def define_of_word (w):
+        if w in data:
+            return data[w]
+        elif len(get_close_matches(w, data.keys()))>0:
+            corr_word = input("Did you mean %s instead? Enter [Y] if yes, or [N} if no: " % get_close_matches(w, data.keys())[0]).upper()
+            if corr_word == "Y":
+                return data[get_close_matches(w, data.keys())[0]]
+            else:
+                return("Word doesn't exist in dictionary.  Please double check spelling.")
+        else:
+            return("Word is not in dictionary")
+    print("\n")
+    for elem in define_of_word(word):
+        print(elem)
+    new_word()
+
+def new_word():
+    try_again = input("\n Would you like new word definition: [y/n] ").lower()
+    print("\n")
+
+    if try_again == "y":
+        main()
+    elif try_again =="n":
+        return("Have a good day!")
+    else:
+        return("Please enter [y/n]" )
+main()
+
+
+
+
+
+
+
