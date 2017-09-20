cc79f340657dc90dc941ee3eddaf1fcd513d2283
Chris W <chris.wiseman74@gmail.com>
2017-09-20 15:15:32 -0700

    Changes to program that will detect facial recognition

--- Python/Computer_vision/face_detection.py
+++ Python/Computer_vision/face_detection.py
@@ -1,14 +1,16 @@
 import cv2
+from cv2 import *
+import numpy as np
 
 face_cascade=cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
-img=cv2.imread("Myphoto.jpg")
+fname='photo.jpg'
+img = imread(fname)
+gray = imread(fname, CV_LOAD_IMAGE_GRAYSCALE)
 
-gray_img+cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+gray = np.array(gray, dtype='uint8')
 
-faces=face_cascade.detectMultiScale(gray_img,
-                                    scaleFactor=1.05,
-                                    minMeighbors=5)
+faces=face_cascade.detectMultiScale(gray, scaleFactor=1.05, minNeighbors=5)
 
 print(type(faces))
 print(faces)
